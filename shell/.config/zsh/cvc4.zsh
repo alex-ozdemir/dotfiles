c10af0fb19eae6203ba19b601c15d7ca12d6d9e6
Alex Ozdemir <aozdemir@hmc.edu>
2019-08-25 14:04:33 -0700

    LFSC compiler aliases

---
+++ shell/.config/zsh/cvc4.zsh
@@ -0,0 +1,14 @@
+alias lfscc="$HOME/repos/LFSC/build/src/lfscc"
+alias cvc4_check="$HOME/repos/LFSC/build/src/lfscc \
+    $HOME/repos/CVC4/proofs/signatures/sat.plf \
+    $HOME/repos/CVC4/proofs/signatures/smt.plf \
+    $HOME/repos/CVC4/proofs/signatures/lrat.plf \
+    $HOME/repos/CVC4/proofs/signatures/drat.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_base.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_bv.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_bv_bitblast.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_arrays.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_int.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_quant.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_real.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_lra.plf"
