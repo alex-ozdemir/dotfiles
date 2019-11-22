7f87c947b95d0e33b4ece1a951a5b6b0c3db729c
Alex Ozdemir <aozdemir@hmc.edu>
2019-11-22 13:31:15 -0800

    Add lfscc alias without arithmetic

--- shell/.config/zsh/cvc4.zsh
+++ shell/.config/zsh/cvc4.zsh
@@ -13,3 +13,12 @@ alias cvc4_check="$HOME/repos/LFSC/build/src/lfscc \
     $HOME/repos/CVC4/proofs/signatures/th_quant.plf \
     $HOME/repos/CVC4/proofs/signatures/th_real.plf \
     $HOME/repos/CVC4/proofs/signatures/th_lra.plf"
+alias lfscc_base="$HOME/repos/LFSC/build/src/lfscc \
+    $HOME/repos/CVC4/proofs/signatures/sat.plf \
+    $HOME/repos/CVC4/proofs/signatures/smt.plf \
+    $HOME/repos/CVC4/proofs/signatures/lrat.plf \
+    $HOME/repos/CVC4/proofs/signatures/drat.plf \
+    $HOME/repos/CVC4/proofs/signatures/er.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_base.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_int.plf \
+    $HOME/repos/CVC4/proofs/signatures/th_real.plf"
