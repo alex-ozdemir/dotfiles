e9e29214d4986e005653787b0a3f10d0e3cdf01e
Alex Ozdemir <aozdemir@hmc.edu>
2021-03-06 20:31:13 -0800

    lfsc: use vars in sig alias

--- shell/.config/zsh/cvc4.zsh
+++ shell/.config/zsh/cvc4.zsh
@@ -1,24 +1,15 @@
 alias lfscc="$HOME/repos/LFSC/build/src/lfscc"
+sig_home="$HOME/repos/signatures/lfsc/old"
 alias cvc4_check="$HOME/repos/LFSC/build/src/lfscc \
-    $HOME/repos/CVC4/proofs/signatures/sat.plf \
-    $HOME/repos/CVC4/proofs/signatures/smt.plf \
-    $HOME/repos/CVC4/proofs/signatures/lrat.plf \
-    $HOME/repos/CVC4/proofs/signatures/drat.plf \
-    $HOME/repos/CVC4/proofs/signatures/er.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_base.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_bv.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_bv_bitblast.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_arrays.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_int.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_quant.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_real.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_lira.plf"
-alias cvc4_check="$HOME/repos/LFSC/build/src/lfscc \
-    $HOME/repos/CVC4/proofs/signatures/sat.plf \
-    $HOME/repos/CVC4/proofs/signatures/smt.plf \
-    $HOME/repos/CVC4/proofs/signatures/lrat.plf \
-    $HOME/repos/CVC4/proofs/signatures/drat.plf \
-    $HOME/repos/CVC4/proofs/signatures/er.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_base.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_int.plf \
-    $HOME/repos/CVC4/proofs/signatures/th_real.plf"
+    $sig_home/sat.plf \
+    $sig_home/smt.plf \
+    $sig_home/lrat.plf \
+    $sig_home/drat.plf \
+    $sig_home/er.plf \
+    $sig_home/th_base.plf \
+    $sig_home/th_bv.plf \
+    $sig_home/th_bv_bitblast.plf \
+    $sig_home/th_arrays.plf \
+    $sig_home/th_int.plf \
+    $sig_home/th_real.plf \
+    $sig_home/th_lira.plf"
