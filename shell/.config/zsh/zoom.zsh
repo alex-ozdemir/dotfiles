174590e4f60d70ae8c8f32a8b51b230239ca5280
Alex Ozdemir <aozdemir@hmc.edu>
2021-12-07 10:41:20 -0800

    zoom cp from i3

--- shell/.config/zsh/zoom.zsh
+++ shell/.config/zsh/zoom.zsh
@@ -1,3 +1,3 @@
-function copy_zoom {
-    echo 'https://stanford.zoom.us/my/aozdemir?pwd=RmdPREZpZnFTN0FyVC83TUhTbmRkUT09' | copy
+function zoom_cp {
+    echo -n 'https://stanford.zoom.us/my/aozdemir?pwd=RmdPREZpZnFTN0FyVC83TUhTbmRkUT09' | copy
 }
