949ff9acc324ac8d38cbd74c1b684465e36c5437
Alex Ozdemir <aozdemir@hmc.edu>
2022-01-18 12:57:38 -0800

    deduplicate in fzo

--- shell/.config/zsh/fzf.zsh
+++ shell/.config/zsh/fzf.zsh
@@ -2,8 +2,8 @@ function fzo {
     directories=(~/resources ~/repos ~/personal ~/stanford)
     if [ $# -gt 0 ]
     then
-        f=$({fd '' $@; fd -I '.*.pdf' $@} | fzf)
+        f=$({fd '' $@; fd -I '.*.pdf' $@} | sort | uniq | fzf)
     else
-        f=$({fd '' $directories; fd -I '.*.pdf' $directories} | fzf)
+        f=$({fd '' $directories; fd -I '.*.pdf' $directories} | sort | uniq | fzf)
     fi && xdg-open $f &
 }
