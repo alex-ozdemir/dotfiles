56e4d6405285564228beb006e9d0cd6c9de9d51a
Veronica Rivera <vrivera@hmc.edu>
2022-01-29 15:44:37 -0800

    Add bibfuzz for fzf'ing cryptobib

--- shell/.config/zsh/cryptobib.zsh
+++ shell/.config/zsh/cryptobib.zsh
@@ -1,3 +1,15 @@
 function search_bib {
     rg -U --multiline-dotall "@\\w*\\{[^@]*$1[^@]*^\\}" ~/repos/cryptobib --no-filename | rg --no-filename -U '\btitle\b|(author =\s*"[^"]*")|@' | rg "$1|^"
 }
+
+function bibfuzz {
+    rg -U --multiline-dotall "@\\w*\\{[^@]*$1[^@]*^\\}" ~/repos/cryptobib --no-filename |
+        rg --no-filename -U '\btitle\b|(author =\s*"[^"]*")|@' |
+        tr '\n' ' ' |
+        sed 's/@/\n@/g' |
+        sed 's/\s\+/ /g'|
+        sed 's/@\w*{\([^,]*\),/\1 :/' |
+        sed 's/\(author = \)\|\(title = \)//g' |
+        fzf |
+        awk '{ print $1 }'
+}
