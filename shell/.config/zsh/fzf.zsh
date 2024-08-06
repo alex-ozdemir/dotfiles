a62b2dc2912c5f31260dff648ceecfab20fcd896
Alex Ozdemir <aozdemir@hmc.edu>
2024-08-06 09:11:42 -0700

    fzf: downloads at depth 1

--- shell/.config/zsh/fzf.zsh
+++ shell/.config/zsh/fzf.zsh
@@ -1,9 +1,10 @@
 function fzo {
     directories=(~/resources ~/repos ~/personal ~/stanford)
+    depth1=(~/Downloads)
     if [ $# -gt 0 ]
     then
         f=$({fd '' $@; fd -I '.*.pdf' $@} | sort | uniq | fzf)
     else
-        f=$({fd '' $directories; fd -I '.*.pdf' $directories} | sort | uniq | fzf)
+        f=$({fd '' $directories; fd -I '.*.pdf' $directories; fd '' -d 1 $depth1} | sort | uniq | fzf)
     fi && xdg-open $f &
 }
